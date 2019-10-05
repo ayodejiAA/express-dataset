162faafb010189bc83201a772764d751e70264e1
ayodejiAA <afolabiayodejia@gmail.com>
2019-10-05 10:07:02 +0100

    feature[actors]: Addd fucntionality

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,36 +1,41 @@
+const Actors = require('../models/Actors');
 
-var getAllActors = () => {
-	
+const getAllActors = async (req, res, next) => {
+  try {
+    const actors = await Actors.getAllActors();
+    res.status(200).json(actors);
+  } catch (error) {
+    next(error);
+  }
 };
 
-var updateActor = () => {
-
+const updateActor = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const actor = await Actors.getById(data.id);
+    if (!actor) return res.status(404).json({});
+    if (actor && actor.login !== data.login) return res.status(400).json({});
+    await Actors.updateAvatar(data.avatar_url, actor.actor_id);
+    res.status(200).json({});
+  } catch (error) {
+    next(error);
+  }
 };
 
-var getStreak = () => {
-
+var getStreak = async (req, res, next) => {
+  try {
+    let actors = await Actors.getStreak();
+    actors.forEach(actor => {
+      delete actor.pushevents;
+    });
+    res.status(200).json(actors);
+  } catch (error) {
+    next(error);
+  }
 };
-
 
 module.exports = {
-	updateActor: updateActor,
-	getAllActors: getAllActors,
-	getStreak: getStreak
+  updateActor: updateActor,
+  getAllActors: getAllActors,
+  getStreak: getStreak
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- models/Actors.js
+++ models/Actors.js
@@ -3,9 +3,44 @@ const Dao = require('../db/dao');
 const db = new Dao();
 
 class Actors {
-  static async findById() {
-    const result = db.get;
+  static async getById(actorId) {
+    const result = await db.get(
+      `SELECT * FROM Actor 
+    WHERE actor_id = ? `,
+      [actorId]
+    );
+    if (!result) return null;
+    return result;
+  }
+
+  static async updateAvatar(avatarUrl, actorId) {
+    const result = await db.run(
+      `UPDATE Actor 
+    SET avatar_url = ? WHERE actor_id = ?`,
+      [avatarUrl, actorId]
+    );
+    return result;
+  }
+
+  static async getAllActors() {
+    const result = await db.all(`
+    SELECT Actor.actor_id as id, Actor.login, Actor.avatar_url FROM  Event
+    JOIN Actor USING (actor_id)
+    GROUP by actor_id
+    ORDER BY count(*) DESC, created_at DESC, login
+  `);
+    return result;
+  }
+  static async getStreak() {
+    const result = await db.all(`
+      SELECT Actor.actor_id as id, Actor.login, Actor.avatar_url,
+      ROW_NUMBER() OVER(PARTITION BY actor_id ORDER BY created_at) AS pushevents
+      FROM  Event
+      JOIN Actor USING (actor_id)
+      ORDER BY pushevents DESC, created_at DESC, login
+  `);
+    return result;
   }
 }
 
-module.exports = Events;
+module.exports = Actors;
